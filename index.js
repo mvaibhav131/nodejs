3f3dd26011c058a42578820b18ec50f48a1129fb
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-06-13 16:24:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,79 @@
-function sum(a,b,print){
-    print (a+b);
-}
-function print(data){
-    console.log(data);
-}
-sum(1,2,print);
-console.log("Hello")
-// its shows the async nature of javascript because 2functions are jump between them ,and its taken time .
 
-const fs=require("fs");
-// const data=fs.readFileSync("./test.txt",{encoding:"utf-8"},);
-// console.log(data);
+const mongoose=require("mongoose");
+const connection=mongoose.connect("mongodb://localhost:27017/IMDB");
+const {Schema,model}=require("mongoose");
 
-fs.readFile("./test.txt",{encoding:"utf-8"},(err,data)=>{
-    if(err){
-        console.error("error occured",err.message)
-    }
-        console.log(data);
+const MovieSchema=new Schema({
+
+    title:{type:String,required:true},
+    rating:Number,
+    releaseDate:Date,
+    earning:{type:Number,default:1000,min:0,max:10000},
+    cast:{type:[String]},
+    language:{
+        type:String,
+        enum:["English","Hindi","Marathi"]
+    },
+});
+
+const Movie=model("movie",MovieSchema);
+
+const main=async()=>{
+    const conn=await connection;
+    console.log("connect success");
+    
+    const movie=new Movie({
+        title:"De Dhakka",
+        rating:4.1,
+        releaseDate:new Date(),
+        earning:7800,
+        cast:["Sidhharth"],
+        language:"Marathi",
     });
-  console.log("Outside File");
+
+    await movie.save();
+    console.log("Saved Movie");
+
+    // const movies=await Movie.find();
+    // console.log("All Movie",movies);
+
+    // const hindimovie=await Movie.find({language:"Hindi"});
+    // console.log("All Hindi Movies",hindimovie);
+
+    conn.disconnect();
+};
+main();
 
 
-  const os=require("os");
-  console.log(os.cpus().length)
-  console.log(os.version());
-  console.log(os.arch());
-  console.log(os.platform());
+
+//express files 
+
+// const express = require("express");
+// const app = express();
+// // const fs = require("fs")
+// const { Movie, connection } = require("./db.js")
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// app.get("/movies", async (req, res) => {
+    
+//     const movies = new Movie.find();
+//     return res.json(movies)
+
+// })
+
+// app.post("/movies", (req, res) => {
+    
+// })
+
+// app.listen(8080, async () => {
+//     try {
+//         await connection;
+//         console.log("connected to db");
+//     }
+//     catch {
+//         console.log("failed to connect");
+//     }
+//    console.log("server started");
+// })
